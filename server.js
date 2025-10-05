c08edba2ecff65fbed1881a42ca15106b4a1e5fa
Chigemezu Emmanuel <chigemezuemmanuel641@gmail.com>
2025-10-05 18:12:33 +0100

    version 2

--- server.js
+++ server.js
@@ -1,30 +1,78 @@
-import express from "express";
-import nodemon from "nodemon";
-import path from "path";
+import express from 'express';
+import path from 'path';
 
 
 const app = express();
-const port = process.env.PORT || 4000;
-const _dirname = path.resolve();
+const port = process.env.PORT || 3040;
+const __dirname = path.resolve();
 
 
-app.use(express.static(path.join(_dirname, "public")));
+app.use(express.static('public'));
+
+
+
+
+const posts = [
+
+
+    {
+
+        id: '1',
+        new_post: "This is my first post.",
+        important: "true",
+
+    },
+
+    {
+
+        id: "2",
+        new_post: "This is my second post",
+        important: "true",
+
+
+    },
+
+    {
+
+        id: "3",
+        new_post: "This is my third post.",
+        important: "true",
+    }
+
+
+];
 
 
 
 
 
-app.get("/", (req, res) => {
 
 
-    const filePath = path.join(_dirname ,"public","Home.html");
+app.get('/', (request, response) => {
+
+    
+
+  response.sendFile(path.join(__dirname, 'public', 'Home.html') );
 
 });
 
 
-app.listen(port, "0.0.0.0", () => {
+app.get('/About', (request, response) => {
+
+response.sendFile(path.join(__dirname, 'public', 'Users', 'sign-up.html'));
+
+});
+
+
+app.get('/users/posts', (request, response) => {
+
+    response.json(posts);
+
+});
+
+
+app.listen(port,'0.0.0.0', () => {
 
     console.log(`Server is running on port ${port}.`);
 
 });
-
